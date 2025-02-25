fd588104d9b19c0545a91d3b91b636838e012faa
fvugt <fvugt@glu.nl>
2025-02-25 21:50:49 +0100

    Fix base URL in Vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/fvugt.github.io/',
+  base: '/',
   plugins: [react(), tailwindcss()],
 }) 
