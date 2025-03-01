29207586406e6352ae0f82a2d2696557b70fdc8f
fvugt <fvugt@glu.nl>
2025-03-01 13:28:00 +0100

    github pages for user/organization

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './App'
 import './App.css'
 
-// Get the base URL from the environment or use a default
-const basename = import.meta.env.BASE_URL
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename={basename}>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 )
