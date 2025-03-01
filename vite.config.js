ecd232a7671e44333e0a28a9985059ffa1c48aff
fvugt <fvugt@glu.nl>
2025-03-01 13:33:17 +0100

    relative paths

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,17 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',  // For GitHub User/Organization Pages, use '/'
+  base: './',  // Use relative paths
   plugins: [react(), tailwindcss()],
+  build: {
+    assetsDir: 'assets',
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name]-[hash].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash].[ext]'
+      }
+    }
+  }
 })
